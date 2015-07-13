b6abc369e1672b0b414d26ef634721f8abcd3435
jlcjensen <coxj12@gmail.com>
2015-07-13 17:10:09 -0400

    Started building other tables for teams and players in NHL.

--- final_app_database_setup.py
+++ final_app_database_setup.py
@@ -15,17 +15,25 @@ class Users(Base):
 	email = Column(String(250), nullable=False)
 	name = Column(String(250), nullable=False)
 
-#TODO finish changing this class over
-class (Base):
-	__tablename__ = ''
 
-	name = Column(String(80), nullable=False)
-	id = Column(Integer, primary_key=True)
+class Team(Base):
+	__tablename__ = 'team'
+
+	tid = Column(Integer, primary_key=True)
+	team_name = Column(String(50), nullable=False)
+
+
+class Players(Base):
+	__tablename__ = 'players'
+
+	fname = Column(String(80), nullable=False)
+	lname = Column(String(80), nullable=False)
+	pid = Column(Integer, primary_key=True)
 	description = Column(String(250))
-	price = Column(String(8))
-	course = Column(String(250))
-	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-	restaurant = relationship(Restaurant)
+	number = Column(String(8))
+	position = Column(String(20))
+	team_id = Column(Integer, ForeignKey('team.tid'))
+	team = relationship(Team)
 
 
 engine = create_engine('sqlite:///restaurant.db')
