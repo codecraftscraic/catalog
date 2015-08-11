d18a22e7d9c0fc74178a712c2ac24933e330f5c9
Jenny Jensen <jlcjensen@gmail.com>
2015-08-11 12:40:36 -0400

    Committing for 1:1 session.

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from final_app_database_setup import Base, Team, Users, Players
@@ -13,12 +13,14 @@ session = DBSession()
 
 ###TO DO: Remember edge case of empty roster or no teams, give option to create
 
+#show all teams in database
 @app.route('/')
 @app.route('/teams/')
 def showTeams():
 	teamList = session.query(Team).all()
 	return render_template('teams.html',teams=teamList)
 
+#see the roster of a given team
 @app.route('/teams/<int:team_id>/', methods=['GET','POST'])
 @app.route('/teams/<int:team_id>/roster/', methods=['GET','POST'])
 def showRoster(team_id):
@@ -26,20 +28,30 @@ def showRoster(team_id):
 	players=session.query(Players).filter(Players.team_id == team_id).all()
 	return render_template('roster.html', team=team, players=players)
 
+#edit the team name
 @app.route('/teams/<int:team_id>/edit/', methods=['GET', 'POST'])
 def editTeam(team_id):
+	editedTeam=session.query(Team).filter(Team.tid == team_id).one()
+	if request.method == 'POST':
+		if request.form['teamname']:
+			editTeam.name = request.form['teamname']
+			session.add(editedTeam)
+			session.commit()
+			flash("Team updated!")
+		return redirect(url_for('showTeams'))
+	else:
+		return render_template('editTeam.html', team = editedTeam)
 
-	return render_template('editTeam.html', Team = team)
+#delete a team from the database
+@app.route('/teams/<int:team_id>/delete/', methods=['GET', 'POST'])
+def deleteTeam(team_id):
+	deleteTeam=session.query(Team).filter(Team.tid == team_id).one()
+	return render_template('deleteTeam.html', team = deleteTeam)
 
 #@app.route('/teams/<int:team_id>/new', methods=['GET', 'POST'])
 #def newTeam(team_id):
 	#return "New team"
 #	return render_template('newTeam.html', Team = team)
-
-#@app.route('/teams/<int:team_id>/delete/', methods=['GET', 'POST'])
-#def deleteTeam(team_id):
-	#return "Delete teams"
-#	return render_template('deleteTeam.html', Team = team)
 
 #@app.route('/teams/<int:team_id>/<int:pid>/new/', methods=['GET', 'POST'])
 #def newPlayer(pid):
